b2a990992b423af358e1776984ad2c0894bfa76a
Özge SEVİM <ozgsvm@gmail.com>
2025-11-22 23:12:38 +0300

    Add page folder and integrate Home, Catalog, and CamperDetails pages using lazy loading with Suspense and Routes

---
+++ src/pages/CamperDetailsPage/CamperDetailsPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import css from "./CamperDetailsPage.module.css";
+
+const CamperDetailsPage = () => {
+  return <div>CamperDetailsPage</div>;
+};
+
+export default CamperDetailsPage;
