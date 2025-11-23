f3cc9e1e7629d3b06850935be3e17a4c6e031263
Özge SEVİM <ozgsvm@gmail.com>
2025-11-23 23:36:11 +0300

    homepage css added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import css from "./HomePage.module.css";
+import NavBar from "../../components/NavBar/NavBar.jsx";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={css.homePageContainer}>
+      <NavBar />
+      <div className={css.homePage}>
+        <div className={css.homePageTitle}>
+          <h1>Campers of your dreams</h1>
+          <h2>You can find everything you want in our catalog</h2>
+        </div>
+        <button className={css.homePageButton}>View Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
