c50f9607965f329ebd743f677be3681712041bb3
Özge SEVİM <ozgsvm@gmail.com>
2025-11-24 21:50:35 +0300

    Add Navbar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import css from "./NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import Logo from "../../assets/Logo.jpg";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className={css.navBarContainer}>
+      <img src={Logo} alt="TravelTrucks" className={css.Logo} />
+      <div className={css.navBar}>
+        <ul className={css.navBarList}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? css.active : css.link)}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/catalog"
+              className={({ isActive }) => (isActive ? css.active : css.link)}
+            >
+              Catalog
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import css from "./CatalogPage.module.css";
+import NavBar from "../../components/NavBar/NavBar";
 
 const CatalogPage = () => {
-  return <div>CatalogPage</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default CatalogPage;
